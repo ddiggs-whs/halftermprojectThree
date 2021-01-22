f7c30d2470dd44f88517abc0042ec918d12615b5
Daniel Diggs <ddiggs@whs.bucks.sch.uk>
2021-01-22 16:03:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+# asdfa
+pygame.init()
+
+DISPLAY = pygame.display.set_mode([800, 600])
+FPSCLOCK = pygame.time.Clock()
+
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+left = 50
+top = 50
+width = 10
+height = 40
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+
+    DISPLAY.fill(black)
+    pygame.draw.rect(DISPLAY, white, [left, top, width, height])
+
+    left += 5
+
+    pygame.display.update()
+    FPSCLOCK.tick(30)
